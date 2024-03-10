1d7ffdbf25d9fb21d93ed5ab415ed2ddfb9cb28e
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-03-10 16:50:09 +0100

    Uploaded the logos

--- src/HomePresentation.tsx
+++ src/HomePresentation.tsx
@@ -1,3 +1,23 @@
+import React from 'react';
+
+const logos = [
+    require("./assets/ff1_logo.png"),
+    /*require("./assets/ff2_logo.png"),
+    require("./assets/ff3_logo.png"),
+    require("./assets/ff4_logo.png"),
+    require("./assets/ff5_logo.png"),
+    require("./assets/ff6_logo.png"),
+    require("./assets/ff7_logo.png"),
+    require("./assets/ff8_logo.png"),
+    require("./assets/ff9_logo.png"),
+    require("./assets/ff10_logo.png"),
+    require("./assets/ff11_logo.png"),
+    require("./assets/ff12_logo.png"),
+    require("./assets/ff13_logo.png"),
+    require("./assets/ff14_logo.png"),
+    require("./assets/ff15_logo.png"),
+    require("./assets/ff16_logo.png"),*/
+]
 
 function HomePresentation() {
     return (
