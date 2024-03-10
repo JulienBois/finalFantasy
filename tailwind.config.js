31a2af024977e68a706950d7e2e19ec90b9bacec
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-03-10 22:52:22 +0100

    Imported animations and new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,22 @@ module.exports = {
     extend: {
         fontFamily: {
             'pixelify': ['pixelify', 'sans'],
-            'finalfantasy': ['finalfantasy', 'finalfantasy'],
             'finalf': ['finalf', 'finalfantasy'],
+            'miriadpro': ['miriadpro', 'sans-serif'],
         },
         animation: {
             'slide-up': 'slide-up 1.2s ease-out',
+            'zoom-in': 'zoom-in 0.5s ease-out forwards',
         },
         keyframes: {
             'slide-up': {
                 '0%': { transform: 'translateY(400%)', opacity: 0},
                 '100%': { transform: 'translateY(0)', opacity: 1},
             },
+            'zoom-in': {
+                '0%': { transform: 'scale(1)', bottom: 0},
+                '100%': { transform: 'scale(1.05)', bottom: 7},
+            }
         }
     },
   },
