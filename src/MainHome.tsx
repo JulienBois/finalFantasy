b6d15a977c9f49690228a504f5fe186013b4586b
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-03-10 22:52:45 +0100

    Added the pictures and new font to the bento UI

--- src/MainHome.tsx
+++ src/MainHome.tsx
@@ -7,16 +7,17 @@ function MainHome() {
                      flex justify-center items-center text-center">Final Fantasy vilains
                 </div>
                 <div
-                    className="rounded-3xl bg-slate-100 border-2 border-gray-200 col-span-3 row-span-3 flex justify-center items-center text-center">
-                    Final Fantasy heroes
+                    className="rounded-3xl bg-slate-100 border-2 border-gray-200 col-span-3 row-span-3 flex justify-center items-center text-center relative">
+                    <img src={require("./assets/ff7_bg.png")} className="object-cover w-full h-full rounded-3xl z-0 absolute" alt="test"/>
+                    <p className="text-8xl absolute w-full flex top-1/3 justify-center z-10 font-miriadpro text-white">Games</p>
+                    <img src={require("./assets/ff7_bg_characters.png")} className="object-cover w-full h-full rounded-3xl z-20 absolute hover:animate-zoom-in"/>
                 </div>
                 <div
                     className="rounded-3xl bg-slate-100 border-2 border-gray-200 col-span-6 row-span-3 flex justify-center items-center text-center">
-                    Final fantasy games
                 </div>
                 <div
                     className="rounded-3xl bg-slate-100 border-2 border-gray-200 col-span-6 row-span-3 flex justify-center items-center text-center">
-                    Final Fantasy music
+                    <img src={require("./assets/final_fantasy_theatrhythm.png")} className="object-cover hover:animate-zoom-in" alt="Final Fantasy theatrhythm"/>
                 </div>
                 <div
                     className="rounded-3xl bg-slate-100 border-2 border-gray-200 col-span-3 row-span-3 flex justify-center items-center text-center">
