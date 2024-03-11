ea07f412d4fdcdb084011f3246203221e91e1532
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-03-11 22:47:31 +0100

    Added the new animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx,jsx}"],
-  theme: {
-    extend: {
-        fontFamily: {
-            'pixelify': ['pixelify', 'sans'],
-            'finalf': ['finalf', 'finalfantasy'],
-            'miriadpro': ['miriadpro', 'sans-serif'],
-        },
-        animation: {
-            'slide-up': 'slide-up 1.2s ease-out',
-            'zoom-in': 'zoom-in 0.5s ease-out forwards',
-        },
-        keyframes: {
-            'slide-up': {
-                '0%': { transform: 'translateY(400%)', opacity: 0},
-                '100%': { transform: 'translateY(0)', opacity: 1},
+    content: ["./src/**/*.{html,js,tsx,jsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                'pixelify': ['pixelify', 'sans'],
+                'finalf': ['finalf', 'finalfantasy'],
+                'miriadpro': ['miriadpro', 'sans-serif'],
             },
-            'zoom-in': {
-                '0%': { transform: 'scale(1)', bottom: 0},
-                '100%': { transform: 'scale(1.05)', bottom: 7},
+            animation: {
+                'slide-up': 'slide-up 1.2s ease-out',
+                'zoom-in': 'zoom-in 0.5s ease-out forwards',
+                'zoom-up': 'zoom-up 0.5s ease-out forwards',
+                'zoom-upper': 'zoom-upper 0.5s ease-out forwards',
+            },
+            keyframes: {
+                'slide-up': {
+                    '0%': {transform: 'translateY(150%)', opacity: 0},
+                    '100%': {transform: 'translateY(0)', opacity: 1},
+                },
+                'zoom-up': {
+                    '0%': {transform: 'scale(1)', bottom: 0},
+                    '100%': {transform: 'scale(1.05)', bottom: 7},
+                },
+                'zoom-upper': {
+                    '0%': {transform: 'scale(1)', bottom: 0},
+                    '100%': {transform: 'scale(1.05)', bottom: 11},
+                },
+                'zoom-in': {
+                    '0%': {transform: 'scale(1)'},
+                    '100%': {transform: 'scale(1.05)'},
+                },
             }
-        }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
+    variants: {
+        extend: {
+            textShadow: ['responsive'],
+        },
+    },
 }
 
