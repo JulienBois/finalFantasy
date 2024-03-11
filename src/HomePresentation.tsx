4fddad202f2f8f5fd30dda271aa04c80ec0bb775
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-03-11 22:47:55 +0100

    Added the white strokes to the title

--- src/HomePresentation.tsx
+++ src/HomePresentation.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 
 const logos = [
-    require("./assets/ff1_logo.png"),
-    require("./assets/ff2_logo.png"),
-    require("./assets/ff3_logo.png"),
-    require("./assets/ff4_logo.png"),
-    require("./assets/ff5_logo.png"),
-    require("./assets/ff6_logo.png"),
-    require("./assets/ff7_logo.png"),
-    require("./assets/ff8_logo.png"),
-    require("./assets/ff9_logo.png"),
-    require("./assets/ff10_logo.png"),
-    require("./assets/ff10-2_logo.png"),
-    require("./assets/ff11_logo.png"),
-    require("./assets/ff12_logo.png"),
-    require("./assets/ff12-2_logo.png"),
-    require("./assets/ff13_logo.png"),
-    require("./assets/ff13-2_logo.png"),
-    require("./assets/ff14_logo.png"),
-    require("./assets/ff15_logo.png"),
-    require("./assets/ff16_logo.png"),
-    require("./assets/ff_dissidia_logo.png"),
+    require("./assets/logos_ff/ff1_logo.png"),
+    require("./assets/logos_ff/ff2_logo.png"),
+    require("./assets/logos_ff/ff3_logo.png"),
+    require("./assets/logos_ff/ff4_logo.png"),
+    require("./assets/logos_ff/ff5_logo.png"),
+    require("./assets/logos_ff/ff6_logo.png"),
+    require("./assets/logos_ff/ff7_logo.png"),
+    require("./assets/logos_ff/ff8_logo.png"),
+    require("./assets/logos_ff/ff9_logo.png"),
+    require("./assets/logos_ff/ff10_logo.png"),
+    require("./assets/logos_ff/ff10-2_logo.png"),
+    require("./assets/logos_ff/ff11_logo.png"),
+    require("./assets/logos_ff/ff12_logo.png"),
+    require("./assets/logos_ff/ff12-2_logo.png"),
+    require("./assets/logos_ff/ff13_logo.png"),
+    require("./assets/logos_ff/ff13-2_logo.png"),
+    require("./assets/logos_ff/ff14_logo.png"),
+    require("./assets/logos_ff/ff15_logo.png"),
+    require("./assets/logos_ff/ff16_logo.png"),
+    require("./assets/logos_ff/ff_dissidia_logo.png"),
 ]
 
 function HomePresentation() {
@@ -29,7 +29,7 @@ function HomePresentation() {
     return (
         <div className="flex justify-center items-center h-screen">
             <img src={randomLogo} className="absolute z-0 animate-flash" alt="Final Fantasy"/>
-            <h1 className="text-xxl font-finalf tracking-widest z-10 animate-slide-up animate-draw-line z-1">FINAL FANTASY</h1>
+            <h1 className="text-xxl font-finalf tracking-widest font-outline-2 z-10 animate-slide-up animate-draw-line z-1">FINAL FANTASY</h1>
         </div>
     );
 }
