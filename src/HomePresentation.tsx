0eb5fe37db205d40c323f683e291497ca76aa497
JulienBois <71647803+JulienBois@users.noreply.github.com>
2024-03-13 11:26:13 +0100

    rearranged assets

--- src/HomePresentation.tsx
+++ src/HomePresentation.tsx
@@ -1,31 +1,11 @@
-import React from 'react';
+import React, {FC} from 'react';
 
-const logos = [
-    require("./assets/ff1_logo.png"),
-    require("./assets/ff2_logo.png"),
-    require("./assets/ff3_logo.png"),
-    require("./assets/ff4_logo.png"),
-    require("./assets/ff5_logo.png"),
-    require("./assets/ff6_logo.png"),
-    require("./assets/ff7_logo.png"),
-    require("./assets/ff8_logo.png"),
-    require("./assets/ff9_logo.png"),
-    require("./assets/ff10_logo.png"),
-    require("./assets/ff10-2_logo.png"),
-    require("./assets/ff11_logo.png"),
-    require("./assets/ff12_logo.png"),
-    require("./assets/ff12-2_logo.png"),
-    require("./assets/ff13_logo.png"),
-    require("./assets/ff13-2_logo.png"),
-    require("./assets/ff14_logo.png"),
-    require("./assets/ff15_logo.png"),
-    require("./assets/ff16_logo.png"),
-    require("./assets/ff_dissidia_logo.png"),
-]
+interface HomePresentationProps {
+    randomLogo: string;
+}
 
-function HomePresentation() {
+const HomePresentation:FC<HomePresentationProps> = ({randomLogo}) => {
 
-    const randomLogo = logos[Math.floor(Math.random() * logos.length)];
     return (
         <div className="flex justify-center items-center h-screen">
             <img src={randomLogo} className="absolute z-0 animate-flash" alt="Final Fantasy"/>
